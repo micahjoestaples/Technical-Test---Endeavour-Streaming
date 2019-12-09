e5208b751bd9f4b8188f59448ae100552b215e41
mstapleton <mstapleton@mdsol.com>
2019-12-09 22:23:34 +0000

    PracticeTestV1

--- cypress/integration/EndDevTest.js
+++ cypress/integration/EndDevTest.js
@@ -6,11 +6,15 @@ context('Actions', () => {
   })
 
   it('.type() - type into a DOM element', () => {
-    cy.get('.gLFyf').type('nba')
+    cy.get('.gLFyf').type('endeavor streaming')
     cy.get(':nth-child(1) > .jKWzZXdEJWi__suggestions-inner-container > .sbtc > .sbl1 > span').click()
-    cy.get('[href="https://www.nba.com/"] > .LC20lb > .S3Uucc').click()
-    cy.contains('Stats')
-    
+    cy.get('[href="https://www.endeavorstreaming.com/"] > .LC20lb > .S3Uucc').click()
+    cy.get('.Header-nav-inner > [href="/about"]').click()
+    cy.contains('INNOVATE')
+
+
+
+
   })
 
 
